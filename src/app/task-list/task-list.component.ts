4bd8ec1a3aaec27d721d157336c614fe4e7c2318
Robert Albus <hello@robertalbus.com>
2019-01-25 16:28:25 -0700

    Fix typo

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -34,7 +34,7 @@ export class TaskList implements OnInit {
     this.addTask(new Task( "Uncomplete this task", "Click on this task to move it to the other side", true));    
     this.addTask(new Task( "Improve Validation", "Show error messages for invalid form", true))
     this.addTask(new Task( "Implement form clearing", "Reset the form inputs and touched-status on a valid submit action", true))
-    this.addTask(new Task( "Poplate lists", "Use one-way databinding to populate lists. *ngIf(task.completed) for sorting to each list.", true));
+    this.addTask(new Task( "Populate lists", "Use one-way databinding to populate lists. *ngIf(task.completed) for sorting to each list.", true));
     this.addTask(new Task( "Create new-task form", "Form for new tasks with name, description, and checkbox for complete-status", true));
     this.addTask(new Task( "Create delete button", "Update task list to include everything except the current task (filter by taskId)", true));
     this.addTask(new Task( "Clean up css", "make list scrollable without losing title", true));
